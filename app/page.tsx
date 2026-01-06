d7aa633c56cee759f6a7aaec7aa48e1ebc6bfc92
Kolfinnr <dmytro1998ua@gmail.com>
2026-01-06 12:17:18 +0100

    Handle missing checkout session URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import DynamicPage from "./[slug]/page";
 
 export default function Home() {
-  return <DynamicPage params={Promise.resolve({ slug: "home" })} />;
+  return <DynamicPage params={{ slug: "home" }} />;
 }
